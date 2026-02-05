b5dee6d5224c07d291e86320eb0d97ece57ebcb9
Rizze <86113452+RizzeDev@users.noreply.github.com>
2026-02-05 19:39:15 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -16,7 +16,6 @@ long_df = df.melt(
 long_df = long_df.dropna()
 long_df["Closing_Rank"] = long_df["Closing_Rank"].astype(int)
 
-
 # ---------------- PERCENTILE ESTIMATION ----------------
 def predict_percentile_from_marks(marks):
     marks = max(0, min(300, marks))
@@ -87,4 +86,5 @@ def predict_cutoff_2026():
 
     predicted_2026 = model.predict([[2026]])[0]
     return round(predicted_2026, 2)
+
 
